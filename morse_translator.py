f8c8fd7398c2960d1dea9b3f12c3a14afdd592a4
danthedolphin <dnltrng@gmail.com>
2018-04-10 16:32:50 -0700

    added repeatability

--- morse_translator.py
+++ morse_translator.py
@@ -2,10 +2,18 @@ alphabet = [chr(x) for x in range(97,123)] #put a-z in a list
 code = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
 morse = dict(zip(alphabet,code)) #map the two lists into a dictionary
 
-msg = str(raw_input('What is your message?\n'))
-msg = msg.replace(' ','').replace('.','').replace('-','').replace(';','').replace('?','').lower() #get rid of the commas periods and make everything lowercase
-key = ''
-for x in msg:
-    key = key + morse[x] + '/'
+while True:
 
-print key
+    msg = str(raw_input('What is your message?\n'))
+    msg = msg.replace(' ','').replace('.','').replace('-','').replace(';','').replace('?','').lower() #get rid of the commas periods and make everything lowercase
+    key = ''
+
+    for x in msg:
+        key = key + morse[x] + '/'
+    print key
+    repeat = raw_input("Do you have another message? y/n\n")
+
+    if repeat == 'n':
+        break
+    else:
+        continue
